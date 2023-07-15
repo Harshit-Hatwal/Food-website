ab2be95f357674f1aa6a2909d87630c3b83c6460
Harshit-Hatwal <harshithatwal@gmail.com>
2023-07-15 20:41:57 +0530

    navbar completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import "./App.css";
+//  app.js mein navbar .js use karna ha toh 
+ // components and app.js same folder mein ha toh ./components
+ import Navbar from "./components/Navbar/Navbar.js";
+
+
+function App() {
+  // writing <Navbar is equivalent to writinng <Navbar> </Navbar> 
+    return <div > 
+      <Navbar />        
+      
+     
+    </div>
+  
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,85 @@
+// making the functional component .
+
+import React from "react";
+// importing the css
+import "./Navbar.css";
+import location from "../../assets/location.png"
+import chevron from "../../assets/chevron.png"
+import  verticalline from "../../assets/verticalline.png"
+import search from "../../assets/search.png" 
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-logo-search-container">
+        {/* Logo  */}
+        <div>
+          <img
+            className="navbar-logo"
+            src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+            alt="Logo"
+          />
+        </div>
+        {/* Search  */}
+        <div className="navbar-search-container">
+          <div className="navbar-search-location">
+            <img
+              className="navbar-search-location"
+              src={location}
+              alt="location"
+            />
+            <input className="navbar-input-location" placeholder="City" />
+            <div>
+              <img
+                className="navbar-search-chevron"
+                src={chevron}
+                alt="chevron"
+              />
+              <img
+                className="navbar-search-vertical-line"
+                src={verticalline}
+                alt="vertical line"
+              />
+            </div>
+          </div>
+          <div className="navbar-search-restaurant">
+            <img
+              className="navbar-search-search-icon"
+              src={search}
+              alt="search"
+            />
+            <input
+              className="navbar-input-restaurant"
+              placeholder="Search for restaurant, cuisine or a dish"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Login Signup  */}
+      <div className="navbar-login-signup-button-container">
+        <button className="navbar-login-button">Log in</button>
+        <button className="navbar-login-button">Sign up</button>
+      </div>
+    </div>
+  );
+}
+  
+// function Navbar()
+// {
+//       // componentname-classname this is the naming convention which 
+//       // i will use in this project . 
+//     return <div className="navbar-container"> 
+
+//     <div >
+//        <img src=" "
+
+//         </img>
+//     </div>
+
+//     </div>;
+   
+
+// }
+
+export default Navbar;
